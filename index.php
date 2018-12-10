9e711109fd38d6383a62aa3c308714b30a730188
Morris <morisi@gmail.com>
2018-12-10 18:59:31 +0400

    git@gitlab.com:Sakheli/news.git

---
+++ index.php
@@ -0,0 +1,369 @@
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>Axlebi.ge</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	
+	<link href="fonts/ionicons.css" rel="stylesheet">		
+	<link href="common/styles.css" rel="stylesheet">
+	
+	
+</head>
+<body>
+	
+	<header>
+	<nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+  <a class="navbar-brand" href="#"><img src="images/logo-white.png" style="width: 160px; height: 42px;"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Recent</a>
+	  </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#">Categories</a>
+	  </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+	</header>
+
+	<br>
+	<br>
+	
+	
+	<div class="container">
+		<div class="h-600x h-sm-auto">
+			<div class="h-2-3 h-sm-auto oflow-hidden">
+		
+				<div class="pb-5 pr-5 pr-sm-0 float-left float-sm-none w-2-3 w-sm-100 h-100 h-sm-300x">
+					<a class="pos-relative h-100 dplay-block" href="#">
+					
+						<div class="img-bg bg-1 bg-grad-layer-6"></div>
+						
+						<div class="abs-blr color-white p-20 bg-sm-color-7">
+							<h3 class="mb-15 mb-sm-5 font-sm-13"><b>Peter Thiels VC Found Invests Million into Bitcoin, Market Reacts</b></h3>
+							<ul class="list-li-mr-20">
+								<li>by <span class="color-primary"><b>Valeri Batiashvili</b></span> Jan 25, 2018</li>
+								<li><i class="color-primary mr-5 font-12 ion-ios-bolt"></i>30,190</li>
+								<li><i class="color-primary mr-5 font-12 ion-chatbubbles"></i>30</li>
+							</ul>
+						</div>
+					</a>
+				</div>
+				
+				<div class="float-left float-sm-none w-1-3 w-sm-100 h-100 h-sm-600x">
+				
+					<div class="pl-5 pb-5 pl-sm-0 ptb-sm-5 pos-relative h-50">
+						<a class="pos-relative h-100 dplay-block" href="#">
+						
+							<div class="img-bg bg-2 bg-grad-layer-6"></div>
+							
+							<div class="abs-blr color-white p-20 bg-sm-color-7">
+								<h4 class="mb-10 mb-sm-5"><b>Russians Bisiops Condems Cryptocurrecy</b></h4>
+								<ul class="list-li-mr-20">
+									<li>Jan 25, 2018</li>
+									<li><i class="color-primary mr-5 font-12 ion-ios-bolt"></i>30,190</li>
+									<li><i class="color-primary mr-5 font-12 ion-chatbubbles"></i>30</li>
+								</ul>
+							</div>
+						</a>
+					</div>
+					
+					<div class="pl-5 ptb-5 pl-sm-0 pos-relative h-50">
+						<a class="pos-relative h-100 dplay-block" href="#">
+						
+							<div class="img-bg bg-3 bg-grad-layer-6"></div>
+							
+							<div class="abs-blr color-white p-20 bg-sm-color-7">
+								<h4 class="mb-10 mb-sm-5"><b>Bitcoin Adoption by Business in 2017</b></h4>
+								<ul class="list-li-mr-20">
+									<li>Jan 25, 2018</li>
+									<li><i class="color-primary mr-5 font-12 ion-ios-bolt"></i>30,190</li>
+									<li><i class="color-primary mr-5 font-12 ion-chatbubbles"></i>30</li>
+								</ul>
+							</div>
+						</a>
+					</div>
+					
+				</div>
+
+			</div>
+			
+			<div class="h-1-3 oflow-hidden">
+		
+				<div class="pr-5 pr-sm-0 pt-5 float-left float-sm-none pos-relative w-1-3 w-sm-100 h-100 h-sm-300x">
+					<a class="pos-relative h-100 dplay-block" href="#">
+					
+						<div class="img-bg bg-4 bg-grad-layer-6"></div>
+						
+						<div class="abs-blr color-white p-20 bg-sm-color-7">
+							<h4 class="mb-10 mb-sm-5"><b>2017 Market Performance: Crypto vs.Stock</b></h4>
+							<ul class="list-li-mr-20">
+								<li>Jan 25, 2018</li>
+								<li><i class="color-primary mr-5 font-12 ion-ios-bolt"></i>30,190</li>
+								<li><i class="color-primary mr-5 font-12 ion-chatbubbles"></i>30</li>
+							</ul>
+						</div>
+					</a>
+				</div>
+				
+				<div class="plr-5 plr-sm-0 pt-5 pt-sm-10 float-left float-sm-none pos-relative w-1-3 w-sm-100 h-100 h-sm-300x">
+					<a class="pos-relative h-100 dplay-block" href="#">
+					
+						<div class="img-bg bg-5 bg-grad-layer-6"></div>
+						
+						<div class="abs-blr color-white p-20 bg-sm-color-7">
+							<h4 class="mb-10 mb-sm-5"><b>Culture Stock: Bitcoin a Part of all Walks of life in 2017</b></h4>
+							<ul class="list-li-mr-20">
+								<li>Jan 25, 2018</li>
+								<li><i class="color-primary mr-5 font-12 ion-ios-bolt"></i>30,190</li>
+								<li><i class="color-primary mr-5 font-12 ion-chatbubbles"></i>30</li>
+							</ul>
+						</div>
+					</a>
+				</div>
+				
+				<div class="pl-5 pl-sm-0 pt-5 pt-sm-10 float-left float-sm-none pos-relative w-1-3 w-sm-100 h-100 h-sm-300x">
+					<a class="pos-relative h-100 dplay-block" href="#">
+					
+						<div class="img-bg bg-6 bg-grad-layer-6"></div>
+						
+						<div class="abs-blr color-white p-20 bg-sm-color-7">
+							<h4 class="mb-10 mb-sm-5"><b>Bitcoin Billionares Hidding in Plain Sight</b></h4>
+							<ul class="list-li-mr-20">
+								<li>Jan 25, 2018</li>
+								<li><i class="color-primary mr-5 font-12 ion-ios-bolt"></i>30,190</li>
+								<li><i class="color-primary mr-5 font-12 ion-chatbubbles"></i>30</li>
+							</ul>
+						</div>
+					</a>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	
+	
+	<section>
+		<div class="container">
+			<div class="row">
+			
+				<div class="col-md-12 col-lg-12">
+					<h4 class="p-title"><b>RECENT NEWS</b></h4>
+					<div class="row">
+					
+						<div class="col-sm-6">
+							<img src="images/recent-news-1-600x450.jpg" alt="">
+							<h4 class="pt-20"><a href="#"><b>2017 Market Performance: <br/>Crypto vs.Stock</b></a></h4>
+							<ul class="list-li-mr-20 pt-10 pb-20">
+								<li class="color-lite-black">by <a href="#" class="color-black"><b>Valeri Batiashvili,</b></a>
+								Jan 25, 2018</li>
+								<li><i class="color-primary mr-5 font-12 ion-ios-bolt"></i><b>30,190</b></li>
+								<li><i class="color-primary mr-5 font-12 ion-chatbubbles"></i><b>47</b></li>
+							</ul>
+							<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
+								doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.</p>
+						</div>
+						
+						<div class="col-sm-6">
+							<a class="oflow-hidden pos-relative mb-20 dplay-block" href="#">
+								<div class="wh-100x abs-tlr"><img src="images/polular-1-100x100.jpg" alt=""></div>
+								<div class="ml-120 min-h-100x">
+									<h5><b>Bitcoin Billionares Hidding in Plain Sight</b></h5>
+									<h6 class="color-lite-black pt-10">by <span class="color-black"><b>Morris Khutsishvili,</b></span> Jan 25, 2018</h6>
+								</div>
+							</a>
+							
+							<a class="oflow-hidden pos-relative mb-20 dplay-block" href="#">
+								<div class="wh-100x abs-tlr"><img src="images/polular-2-100x100.jpg" alt=""></div>
+								<div class="ml-120 min-h-100x">
+									<h5><b>Bitcoin Billionares Hidding in Plain Sight</b></h5>
+									<h6 class="color-lite-black pt-10">by <span class="color-black"><b>Morris Khutsishvili,</b></span> Jan 25, 2018</h6>
+								</div>
+							</a>
+							
+							<a class="oflow-hidden pos-relative mb-20 dplay-block" href="#">
+								<div class="wh-100x abs-tlr"><img src="images/polular-3-100x100.jpg" alt=""></div>
+								<div class="ml-120 min-h-100x">
+									<h5><b>Bitcoin Billionares Hidding in Plain Sight</b></h5>
+									<h6 class="color-lite-black pt-10">by <span class="color-black"><b>Morris Khutsishvili,</b></span> Jan 25, 2018</h6>
+								</div>
+							</a>
+							
+							<a class="oflow-hidden pos-relative mb-20 dplay-block" href="#">
+								<div class="wh-100x abs-tlr"><img src="images/polular-4-100x100.jpg" alt=""></div>
+								<div class="ml-120 min-h-100x">
+									<h5><b>Bitcoin Billionares Hidding in Plain Sight</b></h5>
+									<h6 class="color-lite-black pt-10">by <span class="color-black"><b>Morris Khutsishvili,</b></span> Jan 25, 2018</h6>
+								</div>
+							</a>
+						</div>
+						
+					</div>
+					
+					<h4 class="p-title mt-30"><b>CRYPTO MINING NEWS</b></h4>
+					<div class="row">
+					
+						<div class="col-sm-6">
+							<img src="images/crypto-news-6-600x450.jpg" alt="">
+							<h4 class="pt-20"><a href="#"><b>2017 Market Performance: <br/>Crypto vs.Stock</b></a></h4>
+							<ul class="list-li-mr-20 pt-10 mb-30">
+								<li class="color-lite-black">by <a href="#" class="color-black"><b>Valeri Batiashvili,</b></a>
+								Jan 25, 2018</li>
+								<li><i class="color-primary mr-5 font-12 ion-ios-bolt"></i>30,190</li>
+								<li><i class="color-primary mr-5 font-12 ion-chatbubbles"></i>47</li>
+							</ul>
+						</div>
+						
+						<div class="col-sm-6">
+							<img src="images/crypto-news-5-600x450.jpg" alt="">
+							<h4 class="pt-20"><a href="#"><b>2017 Market Performance: <br/>Crypto vs.Stock</b></a></h4>
+							<ul class="list-li-mr-20 pt-10 mb-30">
+								<li class="color-lite-black">by <a href="#" class="color-black"><b>Valeri Batiashvili,</b></a>
+								Jan 25, 2018</li>
+								<li><i class="color-primary mr-5 font-12 ion-ios-bolt"></i>30,190</li>
+								<li><i class="color-primary mr-5 font-12 ion-chatbubbles"></i>47</li>
+							</ul>
+						</div>
+					
+						<div class="col-sm-6">
+							<img src="images/crypto-news-4-600x450.jpg" alt="">
+							<h4 class="pt-20"><a href="#"><b>2017 Market Performance: <br/>Crypto vs.Stock</b></a></h4>
+							<ul class="list-li-mr-20 pt-10 mb-30">
+								<li class="color-lite-black">by <a href="#" class="color-black"><b>Valeri Batiashvili,</b></a>
+								Jan 25, 2018</li>
+								<li><i class="color-primary mr-5 font-12 ion-ios-bolt"></i>30,190</li>
+								<li><i class="color-primary mr-5 font-12 ion-chatbubbles"></i>47</li>
+							</ul>
+						</div>
+						
+						<div class="col-sm-6">
+							<img src="images/crypto-news-3-600x450.jpg" alt="">
+							<h4 class="pt-20"><a href="#"><b>2017 Market Performance: <br/>Crypto vs.Stock</b></a></h4>
+							<ul class="list-li-mr-20 pt-10 mb-30">
+								<li class="color-lite-black">by <a href="#" class="color-black"><b>Valeri Batiashvili,</b></a>
+								Jan 25, 2018</li>
+								<li><i class="color-primary mr-5 font-12 ion-ios-bolt"></i>30,190</li>
+								<li><i class="color-primary mr-5 font-12 ion-chatbubbles"></i>47</li>
+							</ul>
+						</div>
+						
+						<div class="col-sm-6">
+							<img src="images/crypto-news-2-600x450.jpg" alt="">
+							<h4 class="pt-20"><a href="#"><b>2017 Market Performance: <br/>Crypto vs.Stock</b></a></h4>
+							<ul class="list-li-mr-20 pt-10 mb-30">
+								<li class="color-lite-black">by <a href="#" class="color-black"><b>Valeri Batiashvili,</b></a>
+								Jan 25, 2018</li>
+								<li><i class="color-primary mr-5 font-12 ion-ios-bolt"></i>30,190</li>
+								<li><i class="color-primary mr-5 font-12 ion-chatbubbles"></i>47</li>
+							</ul>
+						</div>
+						
+						<div class="col-sm-6">
+							<img src="images/crypto-news-1-600x450.jpg" alt="">
+							<h4 class="pt-20"><a href="#"><b>2017 Market Performance: Crypto vs.Stock</b></a></h4>
+							<ul class="list-li-mr-20 pt-10 mb-30">
+								<li class="color-lite-black">by <a href="#" class="color-black"><b>Valeri Batiashvili,</b></a>
+								Jan 25, 2018</li>
+								<li><i class="color-primary mr-5 font-12 ion-ios-bolt"></i>30,190</li>
+								<li><i class="color-primary mr-5 font-12 ion-chatbubbles"></i>47</li>
+							</ul>
+						</div>
+						
+					</div>
+					
+					<a class="dplay-block btn-brdr-primary mt-20 mb-md-50" href="#"><b>VIEW MORE</b></a>
+				</div>
+				
+				
+			</div>
+		</div>
+	</section>
+	
+	
+	<footer class="bg-191 color-ccc">
+		
+		<div class="container">
+			<div class="pt-50 pb-20 pos-relative">
+				<div class="abs-tblr pt-50 z--1 text-center">
+					<div class="h-80 pos-relative"><img class="opacty-1 h-100 w-auto" src="images/map.png" alt=""></div>
+				</div>
+				<div class="row">					
+					<div class="col-sm-4">
+						<div class="mb-30">
+							<h5 class="color-primary mb-20"><b>MOST POPULAR</b></h5>
+							<div class="mb-20">
+								<a class="color-white" href="#"><b>Its Make or Break Time for Bitcoin</b></a>
+								<h6 class="mt-10">Jan 25, 2018</h6>
+							</div>
+							<div class="brdr-ash-1 opacty-2 mr-30"></div>
+							<div class="mt-20">
+								<a class="color-white" href="#"><b>Bitcoin's roller coster ride is not over</b></a>
+								<h6 class="mt-10">Jan 25, 2018</h6>
+							</div>
+						</div>
+					</div>
+					
+					<div class="col-sm-4">
+						<div class="mb-30">
+							<h5 class="color-primary mb-20"><b>MOST POPULAR</b></h5>
+							<div class="mb-20">
+								<a class="color-white" href="#"><b>Its Make or Break Time for Bitcoin</b></a>
+								<h6 class="mt-10">Jan 25, 2018</h6>
+							</div>
+							<div class="brdr-ash-1 opacty-2 mr-30"></div>
+							<div class="mt-20">
+								<a class="color-white" href="#"><b>Bitcoin's roller coster ride is not over</b></a>
+								<h6 class="mt-10">Jan 25, 2018</h6>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-4">
+							<h4 class="p-title"><b>NEWSLETTER</b></h4>
+							<p class="mb-20">Subscribe to our newsletter to get notification about new updates,
+								information, discount, etc..</p>
+							<form class="nwsltr-primary-1">
+								<input type="text" placeholder="Your email">
+								<button type="submit"><i class="ion-ios-paperplane"></i></button>
+							</form>
+						</div>			
+			</div>
+			</div>
+			
+			<div class="brdr-ash-1 opacty-2"></div>
+			
+			<div class="oflow-hidden color-ash font-9 text-sm-center ptb-sm-5">
+			
+				<ul class="float-left float-sm-none list-a-plr-10 list-a-plr-sm-5 list-a-ptb-15 list-a-ptb-sm-10">
+					<li><a class="pl-0 pl-sm-10" href="#">Terms & Conditions</a></li>
+					<li><a href="#">Privacy policy</a></li>
+					<li><a href="#">Contact us</a></li>
+				</ul>				
+			</div>
+		</div>
+	</footer>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
